d8a91ab07b7935aa068e26f9c6e0d061a209b597
Gink <kingsley.wong@obf.ateneo.edu>
2017-11-10 18:01:55 +0800

    Increased accuracy of exthook when searching > Changes Ed Sheeran's album named multiply from × to x, which fixed album art finding   for this title.

--- core/exthook.py
+++ core/exthook.py
@@ -1,18 +1,29 @@
+# -*- coding: utf-8 -*-
+import pprint
 import musicbrainzngs
 from musicbrainzngs import ResponseError
+pp = pprint.PrettyPrinter(indent=4)
+
 
 def init():
 	musicbrainzngs.set_useragent("mymusiclist","0.1", contact="none")
 	musicbrainzngs.set_rate_limit(limit_or_interval=False, new_requests=1)
-	print("Exthook init called.")
+	print("[Exthook] Initialized")
 
-def getAlbumArtByName(name, artistname):
-	results = musicbrainzngs.search_release_groups(name, artist=artistname)
+def getAlbumArtByName(name, artist):
+	debug("GET " + name + " " + artist)
+	results = musicbrainzngs.search_release_groups(name, limit = 3, artistname=artist, primarytype="album")
+	#pp.pprint(results['release-group-list'])
 	results = results['release-group-list']
 	resultid = results[0]['id']
-	print("[Exthook] ID =  " + resultid)
+	debug("ID =  " + resultid)
 	try:
 		imagelist = musicbrainzngs.get_release_group_image_list(resultid)
 	except ResponseError:
+		debug("No image found.")
 		return "NaN"
 	return imagelist['images'][0]['thumbnails']['large']
+
+
+def debug(string):
+	print("[Exthook] " + string)
