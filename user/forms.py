daedae1c087b310df7bf229c286364cff72c940f
brian <brian.guadalupe@obf.ateneo.edu>
2017-11-15 19:12:44 +0800

    Add form validation for checking email

--- user/forms.py
+++ user/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=255)
     last_name = forms.CharField(max_length=255)
@@ -11,3 +10,10 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+        if email and User.objects.filter(email=email).exclude(username=username).exists():
+            raise forms.ValidationError('User with this Email address already exists.')
+        return email
