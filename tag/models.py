67c5f2659bd6f2732875c707152cab26aa2fd2a9
brian <brian.guadalupe@obf.ateneo.edu>
2017-12-06 23:15:45 +0800

    Add E

--- tag/models.py
+++ tag/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 from playlist.models import *
 
 class Tag(models.Model):
 	id = models.AutoField(primary_key=True)
 	name = models.CharField(max_length=32)
-	tag = models.ManyToManyField(MusicEntry)
 	def __str__(self):
 		return self.name
+
+class TagEntry(models.Model):
+	tag = models.ForeignKey(Tag)
+	music_entry = models.ForeignKey(MusicEntry)
